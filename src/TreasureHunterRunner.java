6f4cd4e02a5bc120a8f262e43418f1ed7a44eaa8
zheng <maggiez33@nycstudents.net>
2023-12-19 10:39:52 -0500

    complete th1

--- src/TreasureHunterRunner.java
+++ src/TreasureHunterRunner.java
@@ -1,5 +1,6 @@
 public class TreasureHunterRunner {
     public static void main(String[] args) {
+        System.out.println("hi");
         TreasureHunter game = new TreasureHunter();
         game.play();
     }
